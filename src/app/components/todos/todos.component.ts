555f9e1f923ce5d7e36180ae0ce8221a5246cdf9
MarcinBalejko <mbbm9@o2.pl>
2021-03-20 00:41:58 +0100

    :white_check_mark: Added header and submit form components, implemented add todo functionality

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -1,28 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-import {Todo} from '../../models/Todo'
-import {TodoService} from '../../services/todo.service'
+import { Todo } from '../../models/Todo';
+import { TodoService } from '../../services/todo.service';
 
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
-  styleUrls: ['./todos.component.css']
+  styleUrls: ['./todos.component.css'],
 })
 export class TodosComponent implements OnInit {
-  todos:Todo[];
+  todos: Todo[];
 
-  constructor(private todoService:TodoService) { }
+  constructor(private todoService: TodoService) {}
 
   ngOnInit() {
-    this.todoService.getTodos().subscribe(todos => {
-      this.todos = todos
-    })
+    this.todoService.getTodos().subscribe((todos) => {
+      this.todos = todos;
+    });
   }
 
-  deleteTodo(todo:Todo) {
+  deleteTodo(todo: Todo) {
     // Remove From UI
-    this.todos = this.todos.filter(t => t.id !== todo.id);
+    this.todos = this.todos.filter((t) => t.id !== todo.id);
     // Remove from server
     this.todoService.deleteTodo(todo).subscribe();
   }
 
+  addTodo(todo: Todo) {
+    this.todoService.addTodo(todo).subscribe((todo) => {
+      this.todos.push(todo);
+    });
+  }
 }
